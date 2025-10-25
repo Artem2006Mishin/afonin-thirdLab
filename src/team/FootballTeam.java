6192a73088ce0332450c291aba771c199626d00f
mqksrwq <maksim4ikb2006@gmail.com>
2025-10-25 18:32:48 +0300

    added tasks methods in abstract class

--- src/team/FootballTeam.java
+++ src/team/FootballTeam.java
@@ -1,33 +1,75 @@
 package src.team;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 public abstract class FootballTeam {
-  protected static List<FootballTeam> league = new ArrayList<>();
+    protected static List<FootballTeam> league = new ArrayList<>();
 
-  protected String name;
-  protected int wins;
-  
-  protected FootballTeam () {
-    this.name = "Unknown team";
-    this.wins = 0;
-  }
+    protected String name;
+    protected int wins;
 
-  protected FootballTeam (String name, int wins) {
-    this.name = name;
-    this.wins = wins;
-  }
+    protected FootballTeam() {
+        this.name = "Unknown team";
+        this.wins = 0;
+    }
 
-  public String getName () {
-    return this.name;
-  }
+    protected FootballTeam(String name, int wins) {
+        this.name = name;
+        this.wins = wins;
+    }
 
-  public int getWins () {
-    return this.wins;
-  }
+    protected abstract void push(String name, int wins);
 
-  protected abstract void push (String name, int wins);
+    public String getName() {
+        return this.name;
+    }
 
-  // protected abstract void fillTeam ();
+    public int getWins() {
+        return this.wins;
+    }
+
+    public static int averageWins() {
+        int totalWins = 0;
+        for (FootballTeam team : league) {
+            totalWins += team.wins;
+        }
+        return totalWins / league.size();
+    }
+
+    public static String getWinner() {
+        FootballTeam winner = league.get(0);
+        for (FootballTeam team : league) {
+            if (team.getWins() > winner.getWins()) {
+                winner = team;
+            }
+        }
+        return winner.name;
+    }
+
+    public static String getMoreAverageTeams() {
+        int average = averageWins();
+        String output = "";
+        for (FootballTeam team : league) {
+            if (team.wins > average) {
+                output += String.format("Команда: %s, количество побед: %d\n", team.name, team.wins);
+            }
+        }
+        return output;
+    }
+
+    public static void sortTeams() {
+        league.sort(Comparator.comparingInt(FootballTeam::getWins));
+    }
+
+    public static void searchTeam(String name) {
+        for (FootballTeam team : league) {
+            if (team.name.equals(name)) {
+                System.out.println(String.format("Команда: %s, количество побед: %d", team.name, team.wins));
+            } else {
+                System.out.println("Команда не найдена");
+            }
+        }
+    }
 }
